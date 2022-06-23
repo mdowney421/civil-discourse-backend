9ba7db62a240b6dfd1e34464dda058907bf48a5e
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-06-23 17:31:56 -0500

    testing remote db connection

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'matthewdowney',
-    password: '',
-    host: DATABASE_URL,
-    port: 5432,
-    database: 'civil_discourse'
+    connectionString: 'postgres://ebhlymiyhxgstc:9aa83e6c0b992dad4c1316989d5dd47623ef123b74a2382b675692b5032cdb0a@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d597foscs8nk29'
 })
 
 module.exports = pool
